6c799bb27678625dc2b5c9f365b2bfaff9356c24
tunsimp <nguyenquoctuanwar@gmail.com>
2024-05-14 14:11:54 +0700

    App Init

--- MineSweeper.java
+++ MineSweeper.java
@@ -4,5 +4,18 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 public class MineSweeper {
-
+    int tileSize = 70;
+    int numRows = 8;
+    int numCols = numRows;
+    int boardWidth = numCols * tileSize;
+    int boardHeight= numRows * tileSize;
+    JFrame frame = new JFrame("Minesweeper");
+    MineSweeper(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
