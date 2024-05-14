5c4495cdea87c79a537eaea53af9c90180585945
tunsimp <nguyenquoctuanwar@gmail.com>
2024-05-14 14:30:32 +0700

    Adding Name

--- MineSweeper.java
+++ MineSweeper.java
@@ -10,6 +10,8 @@ public class MineSweeper {
     int boardWidth = numCols * tileSize;
     int boardHeight= numRows * tileSize;
     JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
     MineSweeper(){
         frame.setVisible(true);
         frame.setSize(boardWidth,boardHeight);
@@ -17,5 +19,14 @@ public class MineSweeper {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial",Font.BOLD,25));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Minesweeper");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
     }
 }
