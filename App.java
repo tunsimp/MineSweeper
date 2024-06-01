cdb3f8a0f9a853c9b2526b58216cd6284d7755de
quagghyy <148183967+quagghyy@users.noreply.github.com>
2024-06-01 09:50:18 +0700

    add a space for easy to read

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        MineSweeper test=new MineSweeper();
+        MineSweeper test = new MineSweeper();
         test.play();
     }
 }
