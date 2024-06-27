7ccc06047eac8a9b1a2d96d7f9da91c6840d9255
tunsimp <nguyenquoctuanwar@gmail.com>
2024-06-27 14:08:19 +0700

    Delete App.java

--- App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        MineSweeperFrame mineSweeperFrame = MineSweeperFrame.getInstance();
-        mineSweeperFrame.play();
-    }
-}
